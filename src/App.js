166bb395c44c709c15fe516f8fd618a4f23078ac
mouise1111 <mouise1111@gmail.com>
2023-12-12 09:49:14 +0100

    tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
