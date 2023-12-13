780adc5dc903c4dc71bb4c378961f2af9c818810
mouise1111 <mouise1111@gmail.com>
2023-12-13 12:45:35 +0100

    modified ocean color

--- src/components/Ocean.js
+++ src/components/Ocean.js
@@ -2,7 +2,7 @@ import React from "react";
 const Ocean = () => (
   <mesh position-y={-5} rotation-x={-Math.PI * 0.5} scale={10}>
     <planeGeometry args={[30, 30, 50, 50]} />
-    <meshBasicMaterial color={0x68c3c0} flatShading={true} wireframe={false} />
+    <meshBasicMaterial color={0x0065c4} flatShading={true} wireframe={false} />
   </mesh>
 );
 
