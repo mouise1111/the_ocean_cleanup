dbc135a4e2df8079cbaa22ed96f10ff44ee1313d
Rand328 <133091918+Rand328@users.noreply.github.com>
2023-12-13 12:13:34 +0100

    loader page + pop up for navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader from './components/Loader';
+import ArrowKeysPopup from './components/ArrowKeysPopup';
 
-const App = () => (
-  <Canvas>
-    <ambientLight intensity={0.1} />
-    <directionalLight color="red" position={[0, 0, 5]} />
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
-  </Canvas>
-);
+const App = () => {
+  const [transitionTo3D, setTransitionTo3D] = useState(false);
+
+  const handle2DLoaded = () => {
+    // The buttonClick function sets the state to trigger the transition to the 3D website
+  };
+
+  const buttonClick = () => {
+    // Set the state to trigger the transition to the 3D website
+    setTransitionTo3D(true);
+  };
+
+  const handleClosePopup = () => {
+    setTransitionTo3D(true);
+  };
+
+  return (
+    <div>
+      {transitionTo3D ? (
+        // Render the 3D website here
+        <Canvas>
+          {/* Your 3D content goes here */}
+        </Canvas>
+      ) : (
+        // Render the 2D loader screen
+        <Loader onLoaded={handle2DLoaded} buttonClick={buttonClick} />
+      )}
+      {transitionTo3D && <ArrowKeysPopup onClose={handleClosePopup} />}
+    </div>
+  );
+};
 
 export default App;
