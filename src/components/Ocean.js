202e1fc927aabf699c631600cdcafd6ca26740e8
mouise1111 <mouise1111@gmail.com>
2023-12-13 09:00:38 +0100

    created a (simple) Ocean component

---
+++ src/components/Ocean.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Ocean = () => (
+  <mesh position-y={-5} rotation-x={-Math.PI * 0.5} scale={10}>
+    <planeGeometry args={[30, 30, 50, 50]} />
+    <meshBasicMaterial color={0x68c3c0} flatShading={true} wireframe={false} />
+  </mesh>
+);
+
+export default Ocean;
