5ce5c4f3a51d7ee242c838f97234c6eebb35fec6
mouise1111 <mouise1111@gmail.com>
2023-12-13 09:45:12 +0100

    imported boat component to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Ocean from "./components/Ocean.js";
+import Boat from "./components/Boat.js";
 const App = () => (
   <Canvas>
+    <Boat />
     <Ocean />
     <OrbitControls />
   </Canvas>
