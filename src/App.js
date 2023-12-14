24ead5ef54492ce53e7fa88225b7751f49beb686
JDW-ehb <133092166+JDW-ehb@users.noreply.github.com>
2023-12-14 15:01:55 +0100

    linked the database with current folder architecture
    the first database branch worked but the folder system i made didn't make merge really possible so this is a version who can easily be updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import { Canvas } from "@react-three/fiber";
 import Ocean from "./components/Ocean.js";
 import Boat from "./components/Boat.js";
+import React, { useState, useEffect } from "react";
 
-const App = () => (
+const App = () => {
+  
+const [data, setData] = useState([]);
+useEffect(() => {
+  fetch('http://localhost:3030/api/data') // Adjust this endpoint to match your server's route
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error('Error fetching data:', error));
+    console.log("Fetched data:", data); // Check the fetched data
+    setData(data);
+}, []);
+return(
+<div> 
+  <h1>My 3D Scene</h1>
+  <p>This is a paragraph describing the scene.</p>
+
   <Canvas>
     <directionalLight intensity={0.5} />
     <ambientLight />
     <Boat />
     <Ocean />
   </Canvas>
-);
+
+<div>
+    <h2>User Data:</h2>
+    {data.map((user, index) => (
+      <div key={index}>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Score: {user.score}</p>
+      </div>
+    ))}
+  </div>
+
+</div>
+)
+
+};
 
 export default App;
