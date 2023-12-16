470d4ed710f7722988ce62b7c323531fa1a8edda
Rand328 <133091918+Rand328@users.noreply.github.com>
2023-12-16 23:31:17 +0100

    Moving code App.js to HomePage.js in branch StroyTellingPage not merged well so I'm pushing the code from the main

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,16 @@
-// Home.js
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
-import Ocean from "../components/Ocean";
-import Cube from "../components/Cube";
-import { OrbitControls } from '@react-three/drei';
+// HomePage.js
+import { Canvas } from "@react-three/fiber";
+import Ocean from "../components/Ocean.js";
+import Boat from "../components/Boat.js";
+import Story from "../components/Islands/Story.js";
 
 const HomePage = () => (
   <Canvas>
+    <directionalLight intensity={2} />
+    <ambientLight />
+    <Boat />
     <Ocean />
-    <Cube isInHomepage={true} />
-    <OrbitControls />
+    <Story />
   </Canvas>
-);
-
+)
 export default HomePage;
