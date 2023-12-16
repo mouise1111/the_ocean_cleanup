d9242ea1046c79abbc22c46d959901c177468bc2
mouise1111 <mouise1111@gmail.com>
2023-12-16 20:13:56 +0100

    improved boat movement mechanics

--- src/components/Boat.js
+++ src/components/Boat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader, useThree, useFrame } from "@react-three/fiber";
 import { KeyboardControls, PerspectiveCamera } from "@react-three/drei";
-import { Physics, RigidBody } from "@react-three/rapier";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 import Ecctrl, { EcctrlAnimation } from "ecctrl";
 const Boat = () => {
   const { camera } = useThree();
@@ -25,13 +25,19 @@ const Boat = () => {
         rotation-x={0.6}
         rotation-y={Math.PI}
       />
-      <Physics debug={false} timeStep="vary" gravity={[0, 0, 0]}>
+      <Physics debug={false} timeStep="vary">
         <KeyboardControls map={keyboardMap}>
-          {/* Character Control */}
-          <Ecctrl>
-            <primitive object={gltf.scene} position-y={-2.5} scale={1.8} />
+          <Ecctrl sprintMult={2} maxVelLimit={20} turnSpeed={7}>
+            <RigidBody
+              type="kinematicPosition"
+              colliders="hull"
+              position={[0, 5, 0]}
+            >
+              <primitive object={gltf.scene} position-y={-2.5} scale={1.8} />
+            </RigidBody>
           </Ecctrl>
         </KeyboardControls>
+        <CuboidCollider debug position={[0, -2.5, 0]} args={[750, 1, 750]} />
       </Physics>
     </>
   );
