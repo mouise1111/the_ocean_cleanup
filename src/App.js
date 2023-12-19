e33576d3ef87dab61a6157badaef1b23fc9788d4
mouise1111 <mouise1111@gmail.com>
2023-12-19 20:56:34 +0100

    Removed Loader from routes, now the homepage is the default route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import Loader from "./pages/Loader";
 import HomePage from "./pages/Home";
 import StoryTellingPage from "./pages/StoryTelling";
 import LoginPage from "./pages/Auth/Login";
@@ -11,8 +10,7 @@ import RegisterPage from "./pages/Auth/Register";
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Loader />} />
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/story" element={<StoryTellingPage />} />
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/Register" element={<RegisterPage />} />
