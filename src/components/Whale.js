b57df15f130a153ed597f1d497021755cc44ccef
POCCITO <rodelidelon@gmail.com>
2023-12-19 13:47:20 +0100

    Import whale model with animations and play animation

--- src/components/Whale.js
+++ src/components/Whale.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
+import { useAnimations } from "@react-three/drei";
 
 export const Whale = () => {
 	const whale = useLoader(GLTFLoader, "/models/Whale/whale.gltf");
+	const animations = useAnimations(whale.animations, whale.scene)
+
+	useEffect(() => {
+		const action = animations.actions.Swim
+		action.play()
+	}, [])
 
 	return <primitive
 		object={whale.scene}
-		scale={1.2}
+		scale={1.3}
+		position={ [30, 0, 30] }
+		rotation={ [-0.05, 0, 0] }
 		/>;
 };
