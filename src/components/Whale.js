4725f76ec1334220e5a96815898e033cd15eeb63
POCCITO <rodelidelon@gmail.com>
2023-12-19 14:01:53 +0100

    Implementation of whale circle movement

--- src/components/Whale.js
+++ src/components/Whale.js
@@ -1,21 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { useAnimations } from "@react-three/drei";
 
+let angle = 0;
+
+const getCircleMovement = () => {
+  const radius = 0.3;
+  const x = radius * Math.cos(angle);
+  const z = radius * Math.sin(angle);
+  angle += 0.01;
+
+  return {
+    x,
+    z,
+  };
+};
+
 export const Whale = () => {
-	const whale = useLoader(GLTFLoader, "/models/Whale/whale.gltf");
-	const animations = useAnimations(whale.animations, whale.scene)
+  const whale = useLoader(GLTFLoader, "/models/Whale/whale.gltf");
+  const animations = useAnimations(whale.animations, whale.scene);
+  const whaleRef = useRef();
 
-	useEffect(() => {
-		const action = animations.actions.Swim
-		action.play()
-	}, [])
+  useFrame(() => {
+    const circleMovement = getCircleMovement();
+    whaleRef.current.position.x += circleMovement.x;
+    whaleRef.current.position.z += circleMovement.z;
+  });
 
-	return <primitive
-		object={whale.scene}
-		scale={1.3}
-		position={ [30, 0, 30] }
-		rotation={ [-0.05, 0, 0] }
-		/>;
+  useEffect(() => {
+    const action = animations.actions.Swim;
+    action.play();
+  }, [animations]);
+
+  return (
+    <primitive
+      object={whale.scene}
+      scale={1.3}
+      position={[50, 0, 0]}
+      rotation={[-0.05, 0.5, 0]}
+      ref={whaleRef}
+    />
+  );
 };
