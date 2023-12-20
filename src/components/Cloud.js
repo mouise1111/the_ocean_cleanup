920ddd36a8dc1ca0da5fd9a098070a6431e586b3
furqan <furqan.jamil.chaud-ry@student.ehb.be>
2023-12-20 14:38:25 +0100

    Removed cloud animation and ajusted Y axis

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -4,24 +4,15 @@ import { useLoader, useFrame } from "@react-three/fiber";
 import { useAnimations } from "@react-three/drei";
 
 export const Cloud = () => {
-  const cloud = useLoader(GLTFLoader, "/models/cloud/cloud.gltf");
-  const cloudRef = useRef();
+    const cloud = useLoader(GLTFLoader, "/models/cloud/cloud.gltf")
+    const cloudRef = useRef();
 
-  // Use useFrame to animate the cloud's position
-  useFrame((state, delta) => {
-    // Adjust the speed and range of the movement as needed
-    const speed = 0.4;
-    const range = 5;
 
-    // Update the cloud's position based on a sine wave
-    cloudRef.current.position.y = Math.sin(state.clock.elapsedTime * speed) * range;
-  });
-
-  return (
+ return (
     <primitive
       object={cloud.scene}
       scale={1.3}
-      position={[-200, 100, 20]}
+      position={[-100, 0, 70]}
       rotation={[-0.05, 0, 0]}
       ref={cloudRef}
     />
