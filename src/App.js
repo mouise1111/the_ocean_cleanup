6d527e86d55220b3800aa6f4653dda945bbb11e9
mouise1111 <mouise1111@gmail.com>
2023-12-23 10:50:41 +0100

    Remove unused routes from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-// App.js
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
 import HomePage from "./pages/Home";
-import StoryTellingPage from "./pages/StoryTelling";
-import Projects from "./pages/Projects";
-import Donate from "./pages/Donate";
 import LoginPage from "./pages/Auth/Login";
 import RegisterPage from "./pages/Auth/Register";
 
@@ -13,9 +8,6 @@ const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/story" element={<StoryTellingPage />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/donate" element={<Donate />} />
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/Register" element={<RegisterPage />} />
     </Routes>
