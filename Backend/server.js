2ca0f6d97ebb1d0ad65d86ee133bf4e6e5c91f7c
JDW-ehb <133092166+JDW-ehb@users.noreply.github.com>
2023-12-23 15:01:21 +0100

    getting rid of double declarations

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,6 @@ const condb = mysql.createConnection({
   user: 'root',
   password: '',
   database: 'oceanbase'
-  database: 'oceanbase'
 });
 
 
@@ -55,14 +54,12 @@ app.get('/hash', (req, res) => {
 // login API
 app.post('/login', (req, res) => {
   const sql = "SELECT * FROM visitors Where email = ?";
-  const sql = "SELECT * FROM visitors Where email = ?";
   condb.query(sql, [req.body.email], (err, result) => {
       if(err) return res.json({Status: "Error", Error: "Error in runnig query"});
       if(result.length > 0) {
           bcrypt.compare(req.body.password.toString(), result[0].password, (err, response)=> {
               if(err) return res.json({Error: "password error"});
               if(response) {
-                  const token = jwt.sign({role: "gamer"}, "jwt-secret-key", {expiresIn: '1d'});
                   const token = jwt.sign({role: "gamer"}, "jwt-secret-key", {expiresIn: '1d'});
                   return res.json({Status: "Success", Token: token})
               } else {
@@ -79,7 +76,6 @@ app.post('/login', (req, res) => {
 
 // Registration API
 app.post('/register',(req, res) => {
-  const sql = "INSERT INTO visitors (username,email,password) VALUES (?)"; 
   const sql = "INSERT INTO visitors (username,email,password) VALUES (?)"; 
   bcrypt.hash(req.body.password.toString(), 10, (err, hash) => {
       if(err) return res.json({Error: "Error in hashing password"});
