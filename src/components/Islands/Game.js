724db93c6681b0c8e383d4e9ad22754b8f4f92ae
mouise1111 <mouise1111@gmail.com>
2023-12-23 13:44:13 +0100

    repositioned and scaled the game island

--- src/components/Islands/Game.js
+++ src/components/Islands/Game.js
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader, useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import { RigidBody, MeshCollider } from "@react-three/rapier";
-import { useNavigate } from "react-router-dom";
-import { useThree } from "react-three-fiber";
-import { folder, useControls } from 'leva'
-import Enter from "../pop-ups/Enter";
 
-
-const Game = ({sInHomepage}) => {
-    const game = useLoader(GLTFLoader, "/models/islands/game.gltf")
-    return (
+const Game = ({ sInHomepage }) => {
+  const game = useLoader(GLTFLoader, "/models/islands/game.gltf");
+  return (
+    <RigidBody type="fixed">
+      <MeshCollider type="trimesh">
         <primitive
           object={game.scene}
-          scale={1.3}
-          position={[120, 1, 30]}
-          rotation={[0, 15, 0]}
-          
+          scale={2}
+          position={[150, 1, -90]}
+          rotation-y={Math.PI}
         />
-      );
+      </MeshCollider>
+    </RigidBody>
+  );
 };
 
 export default Game;
