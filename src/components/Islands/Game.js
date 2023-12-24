ff418dc98bb303ef5f04e30a8f4dbb5ac7f55e38
Rand328 <133091918+Rand328@users.noreply.github.com>
2023-12-24 11:16:59 +0100

    added pop ups for start and stop the island that appear by interacting with game island. (still just onClick method, needs improvement)

--- src/components/Islands/Game.js
+++ src/components/Islands/Game.js
@@ -1,20 +1,41 @@
+import React, { useState } from "react";
+import { RigidBody, MeshCollider } from "@react-three/rapier";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
-import { RigidBody, MeshCollider } from "@react-three/rapier";
+import StartGame from "../pop-ups/StartGame";
+import Leaderboard from "../pop-ups/Leaderboard";
 
-const Game = ({ sInHomepage }) => {
+const Game = ({ isInHomePage, setPopUpStatus }) => {
   const game = useLoader(GLTFLoader, "/models/islands/game.gltf");
+  const [showStartGame, setShowStartGame] = useState(false);
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+
+  const handleIslandClick = () => {
+    setShowStartGame(true);
+  };
+
   return (
-    <RigidBody type="fixed">
-      <MeshCollider type="trimesh">
-        <primitive
-          object={game.scene}
-          scale={2}
-          position={[150, 1, -90]}
-          rotation-y={Math.PI}
+    <>
+      <RigidBody type="fixed" onClick={handleIslandClick}>
+        <MeshCollider type="trimesh">
+          <primitive
+            object={game.scene}
+            scale={2}
+            position={[150, 1, -90]}
+            rotation-y={Math.PI}
+          />
+        </MeshCollider>
+      </RigidBody>
+      {isInHomePage && showStartGame && (
+        <StartGame
+          setShowStartGame={() => setShowStartGame(false)}
+          setShowLeaderboard={() => setShowLeaderboard(true)}
         />
-      </MeshCollider>
-    </RigidBody>
+      )}
+      {isInHomePage && showLeaderboard && (
+        <Leaderboard setShowLeaderboard={() => setShowLeaderboard(false)} />
+      )}
+    </>
   );
 };
 
