7d0a1233b1c5e7f5e5b11298314fa02cb317e6c0
mouise1111 <mouise1111@gmail.com>
2023-12-24 13:12:52 +0100

    moved the clouds higher on the y-axis by 10

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -3,14 +3,14 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 
 export const Cloud = () => {
-    const cloud = useLoader(GLTFLoader, "/models/cloud/cloud.gltf")
-    const cloudRef = useRef();
+  const cloud = useLoader(GLTFLoader, "/models/cloud/cloud.gltf");
+  const cloudRef = useRef();
 
- return (
+  return (
     <primitive
       object={cloud.scene}
       scale={1.3}
-      position={[-100, -30, 70]}
+      position={[-100, -20, 70]}
       rotation={[-0.05, 0, 0]}
       ref={cloudRef}
     />
