ea63d71f1e725653c9253598f71af227dfb12d41
mouise1111 <mouise1111@gmail.com>
2024-01-02 14:37:59 +0100

    created Iceberg component

---
+++ src/components/Iceberg.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
+import { MeshCollider, RigidBody } from "@react-three/rapier";
+
+export const Iceberg = () => {
+  const iceberg = useLoader(GLTFLoader, "/models/iceberg.gltf");
+
+  return (
+    <RigidBody type="fixed" colliders="hull">
+    <primitive
+      object={iceberg.scene}
+      scale={1.3}
+      position={[-250, 0, -80]}
+      rotation={[0, 7, 0]}
+    />
+    </RigidBody>
+  );
+};
