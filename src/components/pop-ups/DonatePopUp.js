3db0fbfe4e34e3dbfb647a0bd70802889eb880e4
mouise1111 <mouise1111@gmail.com>
2024-01-04 14:12:09 +0100

    donate pop modified

--- src/components/pop-ups/DonatePopUp.js
+++ src/components/pop-ups/DonatePopUp.js
@@ -17,10 +17,19 @@ export const DonatePopUp = ({ onBack }) => {
 
   return (
     <div className="fixed top-0 left-0 w-full p-6 ">
-        <div className="bg-amber-100 bg-opacity-75 w-full max-w-screen-md mx-auto p-5 rounded">
-          <h1 className="text-3xl font-bold mb-4 text-neutral-600">Donate</h1>
-          <p className="text-neutral-600">
-            Text.</p>
+        <div className="w-full max-w-screen-md p-5 mx-auto bg-opacity-75 rounded bg-amber-100">
+          <h1 className="mb-4 text-3xl font-bold">Donate</h1>
+         <h3 className="w-1/2 pb-2 text-2xl font-medium uppercase border-b-2 border-amber-300">Join the cleanup</h3>
+         <h3 className="pb-2 mt-4 text-3xl font-bold uppercase text-amber-800">You can help rid the oceans of plastic</h3>
+        <button
+                type="submit"
+                className="relative flex justify-center w-full px-4 py-2 mt-2 text-sm font-medium text-white transition-colors border border-transparent rounded-md bg-amber-500 group hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-400"
+            
+              >
+                   <a
+                href="https://theoceancleanup.com/donate/"
+              >Donate</a>
+              </button>
         </div>
       </div>
   );
