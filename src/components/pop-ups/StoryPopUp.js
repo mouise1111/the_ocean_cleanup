1647b926feecc9dc602fdebcdc553c287a2f2366
furqan <furqan.jamil.chaud-ry@student.ehb.be>
2024-01-06 22:25:36 +0100

    changed design of donate, story and ocean and river popup

--- src/components/pop-ups/StoryPopUp.js
+++ src/components/pop-ups/StoryPopUp.js
@@ -17,21 +17,27 @@ export const StoryPopUp = ({ onBack }) => {
   }, [onBack]);
 
   return (
-    <div className="fixed top-0 left-0 w-full p-6 ">
-        <div className="bg-amber-100 bg-opacity-75 w-full max-w-screen-md mx-auto p-5 rounded">
-          <h1 className="text-3xl font-bold mb-4 text-neutral-600">Our Story</h1>
-          <p className="text-neutral-600">
-            FOUNDED 2013
-            <br />
-            NON-PROFIT FOUNDATION
-            <br />
-            HQ ROTTERDAM
-            <br />
-            We are fully reliant on donations from individuals, corporations, governments, and institutions.
-            <br />
-            The Ocean Cleanup's team consists of 120 engineers, researchers, scientists, computational modelers, and supporting roles, working daily to rid the world's oceans of plastic.
-          </p>
-        </div>
-      </div>
+<div className="fixed top-0 left-0 w-full "> 
+  <div className="bg-gray-800 bg-opacity-95 w-full max-w-screen-md mx-auto p-8 rounded-lg my-8 py-2 flex items-center">
+    <div className="flex-1">
+      <h1 className="font-inter text-[50px] text-base font-bold mb-4 text-white text-left py-4">Our Story</h1>
+      <p className="font-inter font-light text-[15px] text-white my-4">
+        FOUNDED 2013
+        <br />
+        NON-PROFIT FOUNDATION
+        <br />
+        HQ ROTTERDAM
+        <br />
+        We are fully reliant on donations from individuals, corporations, governments, and institutions.
+        <br />
+        The Ocean Cleanup's team consists of 120 engineers, researchers, scientists, computational modelers, and supporting roles, working daily to rid the world's oceans of plastic.
+      </p>
+    </div>
+    <div className="ml-8">
+      <img src="/pictures/Story_image.png" style={{ width: '220px', height: '220px' }} />
+    </div>
+  </div>
+</div>
+
   );
 };
