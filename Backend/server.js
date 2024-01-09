fabdedd82e9a7578df7a6ca68620a4af88551c04
JDW-ehb <133092166+JDW-ehb@users.noreply.github.com>
2024-01-09 22:55:36 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
